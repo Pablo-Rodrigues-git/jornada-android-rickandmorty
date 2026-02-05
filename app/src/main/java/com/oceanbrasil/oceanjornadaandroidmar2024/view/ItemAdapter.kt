0a1c11431c2bbd8a55e081117705ace9d11af3c5
Pablo Rodrigues <pablo.rodriguesdev@gmail.com>
2026-02-05 20:24:16 -0300

    atualizando projeto com o status do personagem

--- app/src/main/java/com/oceanbrasil/oceanjornadaandroidmar2024/view/ItemAdapter.kt
+++ app/src/main/java/com/oceanbrasil/oceanjornadaandroidmar2024/view/ItemAdapter.kt
@@ -21,6 +21,7 @@ class ItemAdapter(private val itens: List<Item>) : RecyclerView.Adapter<ItemAdap
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val ivImagem: ImageView = itemView.findViewById(R.id.ivImagem)
         val tvNome: TextView = itemView.findViewById(R.id.tvNome)
+        val tvStatus: TextView = itemView.findViewById(R.id.tvStatus)
         val tvDescription: TextView = itemView.findViewById(R.id.tvDescription)
     }
 
@@ -29,7 +30,6 @@ class ItemAdapter(private val itens: List<Item>) : RecyclerView.Adapter<ItemAdap
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         // 1. Inflar o layout XML (`item_layout.xml`) que define a aparência de um item da lista.
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
-        // 2. Criar e retornar um novo ViewHolder, passando a view recém-criada.
         return ViewHolder(view)
     }
 
@@ -40,33 +40,26 @@ class ItemAdapter(private val itens: List<Item>) : RecyclerView.Adapter<ItemAdap
         val item = itens[position]
         // 2. Define o texto do TextView `tvNome` com o nome do personagem.
         holder.tvNome.text = item.nome
-        // 3. Define o texto da descrição concatenando o status e a espécie do personagem.
-        holder.tvDescription.text = "${item.status} - ${item.species}"
+        // 3 define o status do personagem
+        holder.tvStatus.text = item.status
+        // 4. define a especie do personagem
+        holder.tvDescription.text = item.species
 
         // Lógica para gerar uma cor única para cada nome de personagem.
         val color = Color.HSVToColor(floatArrayOf((item.nome.hashCode() % 360).toFloat(), 0.8f, 0.9f))
         holder.tvNome.setTextColor(color)
 
-        // 4. Usa a biblioteca Glide para carregar a imagem a partir da URL (`item.imagem`)
-        // e exibi-la no `ivImagem` do ViewHolder.
         Glide.with(holder.itemView.context)
             .load(item.imagem)
             .into(holder.ivImagem)
 
-        // 5. Define um listener de clique para a view do item.
-        // Quando um item da lista é tocado, este código é executado.
         holder.itemView.setOnClickListener {
-            // Cria uma Intent para navegar da tela atual (`it.context`) para a `ItemDetailActivity`.
             val intent = Intent(it.context, ItemDetailActivity::class.java)
-            // Adiciona o ID do item clicado como um "extra" na Intent, para que a próxima tela saiba qual personagem exibir.
             intent.putExtra("ID", item.id)
-            // Inicia a nova activity.
             it.context.startActivity(intent)
         }
     }
 
-    // `getItemCount` informa ao RecyclerView o número total de itens na lista.
-    // O RecyclerView usa este número para saber quantos itens ele precisa desenhar.
     override fun getItemCount(): Int {
         return itens.size
     }
